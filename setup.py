a6ab9e2624e10c67ca5bd0c4e84661f85dc7eea3
kasenjing <72774380+kasenjing@users.noreply.github.com>
2024-03-15 19:28:23 +0800

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, Extension
 import setuptools_scm  # noqa  Ensure it’s installed
 
 extensions = [
-    Extension("cutadapt._align", sources=["src/cutadapt/_align.pyx"]),
-    Extension("cutadapt.qualtrim", sources=["src/cutadapt/qualtrim.pyx"]),
-    Extension("cutadapt.info", sources=["src/cutadapt/info.pyx"]),
-    Extension("cutadapt._kmer_finder", sources=["src/cutadapt/_kmer_finder.pyx"]),
+    Extension("cutadapt2._align", sources=["src/cutadapt2/_align.pyx"]),
+    Extension("cutadapt2.qualtrim", sources=["src/cutadapt2/qualtrim.pyx"]),
+    Extension("cutadapt2.info", sources=["src/cutadapt2/info.pyx"]),
+    Extension("cutadapt2._kmer_finder", sources=["src/cutadapt2/_kmer_finder.pyx"]),
 ]
 
 setup(ext_modules=extensions)
